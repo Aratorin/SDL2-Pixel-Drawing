a4bfbaebffb5a58b9c59da9f3ad9587448cf11be
Aratorin <johnjhofmann3@gmail.com>
2020-07-22 21:31:26 -0400

    Smoother drawing and larger canvas.
    - Changed SDL_RenderFlags from SDL_RENDERER_PRESENTVSYNC to
      SDL_RENDERER_ACCELERATED to drastically improve FPS, resulting in less
      gaps between pixels.
    - Removed  SDL_RenderClear(renderer), as it servers no purpose in this
      program. Removing it increased the FPS even more.
    - Moved window width and heigth into a variable, and increased the size
      of the window.

--- SDL2 Pixel Drawing/SDL2 Pixel Drawing.cpp
+++ SDL2 Pixel Drawing/SDL2 Pixel Drawing.cpp
@@ -4,24 +4,25 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	bool leftMouseButtonDown = false;
-	bool quit = false;
 	SDL_Event event;
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("SDL2 Pixel Drawing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+	const int SDL_WINDOW_WIDTH = 800;
+	const int SDL_WINDOW_HEIGHT = 600;
+	SDL_Window* window = SDL_CreateWindow("SDL2 Pixel Drawing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOW_WIDTH, SDL_WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 640, 480);
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, SDL_WINDOW_WIDTH, SDL_WINDOW_HEIGHT);
 
-	int* pixels = new int[640 * 480 * sizeof(Uint32)];
+	int* pixels = new int[SDL_WINDOW_WIDTH * SDL_WINDOW_HEIGHT * sizeof(Uint32)];
 
-	memset(pixels, 255, 640 * 480 * sizeof(Uint32));
+	memset(pixels, 255, SDL_WINDOW_WIDTH * SDL_WINDOW_HEIGHT * sizeof(Uint32));
 
 	Uint32 color;
 	Uint8 red, green, blue;
 
 
-	while (!quit) {
+	while (true) {
 		int ticks = SDL_GetTicks();
 		red = (1 + sin(ticks * 0.0002)) * 128;
 		green = (1 + sin(ticks * 0.0001)) * 128;
@@ -33,11 +34,11 @@ int main(int argc, char* argv[]) {
 		color += green;
 		color <<= 8;
 		color += blue;
-		SDL_UpdateTexture(texture, NULL, pixels, 640 * sizeof(Uint32));
+		SDL_UpdateTexture(texture, NULL, pixels, SDL_WINDOW_WIDTH * sizeof(Uint32));
 		SDL_WaitEvent(&event);
 		switch (event.type) {
 		case SDL_QUIT:
-			quit = true;
+			return 0;
 			break;
 		case SDL_MOUSEBUTTONUP:
 			if (event.button.button == SDL_BUTTON_LEFT) {
@@ -56,26 +57,16 @@ int main(int argc, char* argv[]) {
 					for (int j = -2; j < 3; j++) {
 						int x = mouseX + i;
 						int y = mouseY + j;
-						if (x < 0 || x>640 || y < 0 || y>480) {
+						if (x < 0 || x>SDL_WINDOW_WIDTH || y < 0 || y>SDL_WINDOW_HEIGHT) {
 							continue;
 						} else {
-							pixels[(mouseX + i) + ((mouseY + j) * 640)] = color;
+							pixels[x + (y * SDL_WINDOW_WIDTH)] = color;
 						}
 					}
 				}
-				/*pixels[mouseX + (mouseY * 640)] = 0;
-				pixels[(mouseX + 1) + (mouseY * 640)] = 0;
-				pixels[(mouseX - 1) + (mouseY * 640)] = 0;
-				pixels[mouseX + ((mouseY + 1) * 640)] = 0;
-				pixels[(mouseX + 1) + ((mouseY + 1) * 640)] = 0;
-				pixels[(mouseX - 1) + ((mouseY + 1) * 640)] = 0;
-				pixels[mouseX + ((mouseY - 1) * 640)] = 0;
-				pixels[(mouseX + 1) + ((mouseY - 1) * 640)] = 0;
-				pixels[(mouseX - 1) + ((mouseY - 1) * 640)] = 0;*/
 			}
 			break;
 		}
-		SDL_RenderClear(renderer);
 		SDL_RenderCopy(renderer, texture, NULL, NULL);
 		SDL_RenderPresent(renderer);
 	}
